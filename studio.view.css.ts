38c0e0a2e3d7c1f5615580606225fbd21ec9bbfb
jin <jin@hyoo.ru>
2021-06-28 01:15:51 +0800

    mvp

---
+++ studio.view.css.ts
@@ -0,0 +1,27 @@
+namespace $.$$ {
+	
+	const { rem } = $mol_style_unit
+	
+	$mol_style_define( $hyoo_mol_studio, {
+		
+		Edit: {
+			flex: {
+				basis: rem(40),
+				shrink: 0,
+			},
+		},
+		
+		Edit_form: {
+			display: 'flex',
+		},
+		
+		Preview: {
+			flex: {
+				basis: rem(40),
+				shrink: 0,
+			},
+		},
+		
+	} )
+	
+}
---
+++ studio.view.ts
@@ -0,0 +1,99 @@
+namespace $.$$ {
+	
+	export class $hyoo_mol_studio extends $.$hyoo_mol_studio {
+		
+		@ $mol_mem
+		pack( next?: string ) {
+			return this.$.$mol_state_arg.value( 'pack', next ) ?? super.pack()
+		}
+		
+		@ $mol_mem
+		source( next?: string ) {
+			return this.$.$mol_state_arg.value( 'source', next ) ?? super.source()
+		}
+		
+		@ $mol_mem
+		tree( next?: $mol_tree2 ) {
+			const source = this.source( next && next.toString() )
+			return this.$.$mol_view_tree2_classes(
+				this.$.$mol_tree2_from_string( source )
+			).kids[0]
+		}
+		
+		@ $mol_mem
+		self( next?: string ) {
+			
+			const tree = this.tree()
+			if( !next ) return tree.type
+			
+			this.tree(
+				tree.struct( next, tree.kids )
+			)
+			
+			return next
+		}
+		
+		@ $mol_mem
+		base( next?: string ) {
+			
+			const self = this.tree()
+			const base = this.$.$mol_view_tree2_class_super( self )
+			if( !next ) return base.type
+			
+			this.tree(
+				self.clone([
+					base.struct( next, base.kids )
+				])
+			)
+			
+			return next
+		}
+		
+		@ $mol_mem
+		preview_html() {
+			
+			const self = this.self()
+			const script = new URL( 'web.js', this.pack() ).toString()
+			
+			return `
+				<html style="height:100%;width:100%">
+					<body style="margin:0;height:100%;width:100%">
+						<script src="${ script }"></script>
+						<script>${ this.self_code() }</script>
+						<div mol_view_root="${ self }"></div>
+						<script>$mol_view.autobind()</script>
+					</body>
+				</html>
+			`
+			
+		}
+		
+		@ $mol_mem
+		self_code() {
+			
+			const tree = this.tree()
+			
+			const code = this.$.$mol_tree2_text_to_string_mapped_js(
+				this.$.$mol_tree2_js_to_text(
+					this.$.$mol_view_tree2_to_js(
+						tree.list([ tree ])
+					)
+				)
+			)
+			
+			return `
+				$.${ this.self() } = ${ code }
+			`
+		}
+		
+		@ $mol_mem
+		base_options() {
+			const win = this.Preview().window()
+			return Object.keys( win )
+				.filter( name => typeof win[name] === 'function' )
+				.filter( name => win[name].prototype instanceof win['$mol_view'] )
+		}
+		
+	}
+	
+}
