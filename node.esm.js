100662c0da7ccdfb55e7fc2e8936fd618998441c
nin-jin <nin-jin@users.noreply.github.com>
2021-09-15 07:12:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -6296,6 +6296,7 @@ var $;
             obj.checked = (val) => this.option_checked(id, val);
             obj.label = () => this.option_label(id);
             obj.enabled = () => this.option_enabled(id);
+            obj.hint = () => this.option_hint(id);
             obj.minimal_height = () => 24;
             return obj;
         }
@@ -6331,6 +6332,9 @@ var $;
         }
         option_enabled(id) {
             return this.enabled();
+        }
+        option_hint(id) {
+            return "";
         }
         items() {
             return [];
