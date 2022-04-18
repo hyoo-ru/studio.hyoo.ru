b77ded6363b602d59bf3ce4e785d3c817e7524c9
nin-jin <nin-jin@users.noreply.github.com>
2022-04-18 07:19:44 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1197,7 +1197,7 @@ declare namespace $ {
         disabled(): boolean;
         tab_index(): number;
         hint(): string;
-        hint_or_error(): string;
+        error(): string;
     }
 }
 
@@ -1317,7 +1317,6 @@ declare namespace $.$$ {
         event_key_press(event: KeyboardEvent): void;
         tab_index(): number;
         error(): string;
-        hint_or_error(): string;
         sub_visible(): ($mol_view_content | $mol_speck)[];
     }
 }
@@ -2450,15 +2449,11 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_pop extends $mol_view {
-        event(): {
-            keydown: (event?: any) => any;
-        };
         showed(val?: any): boolean;
         align_vert(): string;
         align_hor(): string;
         sub(): readonly any[];
         sub_visible(): readonly any[];
-        keydown(event?: any): any;
         Anchor(): any;
         align(): string;
         bubble_content(): readonly $mol_view_content[];
@@ -2491,13 +2486,16 @@ declare namespace $.$$ {
         align(): string;
         align_vert(): "suspense" | "top" | "bottom";
         align_hor(): "suspense" | "left" | "right";
-        keydown(event: KeyboardEvent): void;
     }
 }
 
 declare namespace $ {
     class $mol_pick extends $mol_pop {
+        event(): {
+            keydown: (event?: any) => any;
+        };
         Anchor(): $$.$mol_check;
+        keydown(event?: any): any;
         trigger_enabled(): boolean;
         trigger_content(): readonly $mol_view_content[];
         hint(): string;
@@ -2506,6 +2504,12 @@ declare namespace $ {
 }
 
 declare namespace $ {
+}
+
+declare namespace $.$$ {
+    class $mol_pick extends $.$mol_pick {
+        keydown(event: KeyboardEvent): void;
+    }
 }
 
 declare namespace $ {
--- web.d.ts
+++ web.d.ts
@@ -1165,7 +1165,7 @@ declare namespace $ {
         disabled(): boolean;
         tab_index(): number;
         hint(): string;
-        hint_or_error(): string;
+        error(): string;
     }
 }
 
@@ -1285,7 +1285,6 @@ declare namespace $.$$ {
         event_key_press(event: KeyboardEvent): void;
         tab_index(): number;
         error(): string;
-        hint_or_error(): string;
         sub_visible(): ($mol_view_content | $mol_speck)[];
     }
 }
@@ -2463,15 +2462,11 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_pop extends $mol_view {
-        event(): {
-            keydown: (event?: any) => any;
-        };
         showed(val?: any): boolean;
         align_vert(): string;
         align_hor(): string;
         sub(): readonly any[];
         sub_visible(): readonly any[];
-        keydown(event?: any): any;
         Anchor(): any;
         align(): string;
         bubble_content(): readonly $mol_view_content[];
@@ -2504,13 +2499,16 @@ declare namespace $.$$ {
         align(): string;
         align_vert(): "suspense" | "top" | "bottom";
         align_hor(): "suspense" | "left" | "right";
-        keydown(event: KeyboardEvent): void;
     }
 }
 
 declare namespace $ {
     class $mol_pick extends $mol_pop {
+        event(): {
+            keydown: (event?: any) => any;
+        };
         Anchor(): $$.$mol_check;
+        keydown(event?: any): any;
         trigger_enabled(): boolean;
         trigger_content(): readonly $mol_view_content[];
         hint(): string;
@@ -2519,6 +2517,12 @@ declare namespace $ {
 }
 
 declare namespace $ {
+}
+
+declare namespace $.$$ {
+    class $mol_pick extends $.$mol_pick {
+        keydown(event: KeyboardEvent): void;
+    }
 }
 
 declare namespace $ {
