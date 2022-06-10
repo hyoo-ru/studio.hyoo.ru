b8db9d90f514dbfe5de1cbb7daaa8f88730a7424
nin-jin <nin-jin@users.noreply.github.com>
2022-06-10 07:20:23 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1736,6 +1736,7 @@ declare namespace $ {
         'code-string': RegExp;
         'code-number': RegExp;
         'code-call': RegExp;
+        'code-sexpr': RegExp;
         'code-field': RegExp;
         'code-keyword': RegExp;
         'code-global': RegExp;
