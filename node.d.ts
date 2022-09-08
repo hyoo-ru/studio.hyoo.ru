c5d6c801d34df698dd726088758608d559c8ee96
nin-jin <nin-jin@users.noreply.github.com>
2022-09-08 07:34:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -940,7 +940,7 @@ declare namespace $ {
     class $mol_form_field extends $mol_labeler {
         bids(): readonly string[];
         label(): readonly any[];
-        content(): readonly any[];
+        Content(): any;
         name(): string;
         bid(): string;
         Bid(): $mol_view;
@@ -2908,7 +2908,7 @@ declare namespace $ {
             src: string;
             srcdoc: any;
             allow: string;
-            allowFullscreen: boolean;
+            allowfullscreen: boolean;
         };
         accelerometer(): boolean;
         autoplay(): boolean;
