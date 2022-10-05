5833cc1e27e9e85eeeec6961ade50bad483e126e
nin-jin <nin-jin@users.noreply.github.com>
2022-10-05 07:35:38 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -6443,6 +6443,9 @@ var $;
                 return val;
             return [];
         }
+        bring() {
+            return this.Edit().bring();
+        }
         Edit() {
             const obj = new this.$.$mol_textarea_edit();
             obj.value = (val) => this.value(val);
--- web.d.ts
+++ web.d.ts
@@ -1980,6 +1980,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
         highlight(): string;
