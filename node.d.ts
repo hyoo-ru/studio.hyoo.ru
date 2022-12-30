dd69d51b18f387e76169d50837543eb444207025
nin-jin <nin-jin@users.noreply.github.com>
2022-12-30 07:15:31 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -3145,6 +3145,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_form extends $mol_list {
+        submit_allowed(): boolean;
         submit_blocked(): boolean;
         event(): {
             keydown: (event?: any) => any;
@@ -3159,9 +3160,6 @@ declare namespace $ {
         foot(): readonly $mol_view[];
         Foot(): $mol_row;
     }
-}
-
-declare namespace $ {
 }
 
 declare namespace $.$$ {
--- web.d.ts
+++ web.d.ts
@@ -3063,6 +3063,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_form extends $mol_list {
+        submit_allowed(): boolean;
         submit_blocked(): boolean;
         event(): {
             keydown: (event?: any) => any;
@@ -3077,9 +3078,6 @@ declare namespace $ {
         foot(): readonly $mol_view[];
         Foot(): $mol_row;
     }
-}
-
-declare namespace $ {
 }
 
 declare namespace $.$$ {
