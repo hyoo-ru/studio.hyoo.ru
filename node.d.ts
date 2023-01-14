9df27689fe3ab55c7f82b97e422aa594e79359f2
nin-jin <nin-jin@users.noreply.github.com>
2023-01-14 07:14:49 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -3181,6 +3181,9 @@ declare namespace $ {
     }
 }
 
+declare namespace $ {
+}
+
 declare namespace $.$$ {
     class $mol_form extends $.$mol_form {
         form_fields(): readonly $mol_form_field[];
