a9a1c8c7719340df907c8c218f857a994f7d4531
nin-jin <nin-jin@users.noreply.github.com>
2023-04-22 07:14:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -14801,12 +14801,18 @@ var $;
         string_enabled() {
             return this.enabled();
         }
+        submit(next) {
+            if (next !== undefined)
+                return next;
+            return null;
+        }
         String() {
             const obj = new this.$.$mol_string();
             obj.type = () => this.type();
             obj.value = (val) => this.value_string(val);
             obj.hint = () => this.hint();
             obj.enabled = () => this.string_enabled();
+            obj.submit = (next) => this.submit(next);
             return obj;
         }
         event_dec(next) {
@@ -14858,6 +14864,9 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_number.prototype, "value_string", null);
+    __decorate([
+        $mol_mem
+    ], $mol_number.prototype, "submit", null);
     __decorate([
         $mol_mem
     ], $mol_number.prototype, "String", null);
